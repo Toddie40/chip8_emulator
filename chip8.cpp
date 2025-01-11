bca15c3211a4e8218a708d8600f9bf7b954a8ecc
Alexander Todd <Alex.todd40@gmail.com>
2025-01-11 22:03:01 +0000

    Rest of opcodes implemented (exclusiding input one)

--- chip8.cpp
+++ chip8.cpp
@@ -4,12 +4,13 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <random>
 
 // a class for our chip8
 class Chip8 
 {
     // we need somewhere to store our current opcode. 
-    unsigned short currentOpcode;
+    unsigned short currentOpcode = 0;
 
     /*  
         We need some memory allocated to our chip8
@@ -17,7 +18,7 @@ class Chip8
         The memory  in location 0x000 to 0x1ff (0-511) is saved for the interpreter but in our case we shall use it for font storage
         This will matter when we write the function to load our program. We need to make sure to load it into the correct bit of memory 
     */
-    unsigned char memory[4096];
+    unsigned char memory[4096] = { 0 };
 
     /*
         The chip8 has a display of resolution 64 x 32 and is monochrome. 
@@ -31,35 +32,35 @@ class Chip8
     /*
         The program counter which points to the currenty instruction in memory. This is then always just a memory address. i.e. an integer between 0 and 4095 (since that's the size memory we have)
     */
-    unsigned short programCounter;
+    unsigned short programCounter = 0;
     
     /*
         The index register is a 16 bit register that is used for operations related to reading and writing memory. 
     */
-    unsigned short indexRegister;
+    unsigned short indexRegister = 0;
 
     /*
         General purpose variable regsiters. Each 2 bytes long. There are 16 of them lablled V0 to VF with VF being used as a flag resister
     */
 
-    unsigned short vRegister[16];
+    unsigned short vRegister[16] = {0};
 
-    unsigned char delayTimer;
+    unsigned char delayTimer = 0;
 
-    unsigned char soundTimer;
+    unsigned char soundTimer = 0;
 
     /*
         We also need a stack so that we can enable subroutines and jumping
     */
 
-    unsigned short stack[16];
-    unsigned short stackPointer;
+    unsigned short stack[16] = { 0 };
+    unsigned short stackPointer = 0;
 
     /*
         And finally a variable to store which keys are currently being pressed. The chip8 has 16 keys labelled 0-F
     */
 
-    unsigned char key[16];
+    unsigned char key[16] = { 0};
 
     unsigned int disp_index = 0;
 
@@ -112,12 +113,14 @@ class Chip8
                 
                 // draw the display (need to do this only 60 times a second)
                 if (gpu_update > 1.0f / 60.0f) {
-                    DrawGraphics();
+                    DrawGraphics();                    
                     gpu_update = 0;
                 }
                 
                 // update timers
                 float delta_t = (SDL_GetTicks() - previous_time) / 1000.0f;
+                
+                previous_time = SDL_GetTicks(); //update previous time for next tick
                 step_update += delta_t;
                 gpu_update += delta_t;
             }
@@ -125,6 +128,18 @@ class Chip8
         
 
     private:
+
+        // random byte generator function
+        unsigned char RandomByte() {
+            std::random_device rd;
+            std::mt19937 gen(rd()); // using the mt19937 generator
+             // generate a random number between 0 and 255
+             // remembering that a byte is effectively any number betwwen 0 and 255
+             // since 2^8 = 256
+            std::uniform_int_distribution<> dis(0, 255);
+            // generaqte a number and cast it to an unsigned char
+            return static_cast<unsigned char>(dis(gen));
+        }
 
         void InitialiseGraphics() {
             // draw empty display to the screen            
@@ -211,6 +226,11 @@ class Chip8
                             }
                             break;
                         }
+                        case 0x00EE: { // RET - return from subroutine. set PC to address at the top fot he stack and then subtract 1 from stack pointer
+                            programCounter = stack[stackPointer];
+                            stackPointer--;
+                            break;
+                        }
                         default: {
                             // call RCA 1802 program at address NNN
                             // not implemented
@@ -223,16 +243,106 @@ class Chip8
                     programCounter = third_byte;
                     break;
                 }
+                case 0x2: { // Call addr call subroutine at nnn. increment stack pointer, put current PC on top. set PC to nnn
+                    stackPointer++;
+                    stack[stackPointer] = programCounter;
+                    programCounter = third_byte;
+                    break;
+                }
+                case 0x3: { // 3xkk - skip next instruction if Vc = kk
+                    if (vRegister[nibble_2]  == second_byte) {
+                        programCounter += 2;                        
+                    }
+                    break;
+                }
+                case 0x4: { // 4xkk - skip next instruction if Vx not equal to kk
+                    if (vRegister[nibble_2] != second_byte) {
+                        programCounter += 2;
+                    }
+                    break;
+                }
+                case 0x5: { // 5xy0 skip next instruction if vx == vy
+                    if (vRegister[nibble_2] == vRegister[nibble_3]) {
+                        programCounter += 2;
+                    }
+                    break;
+                }
                 case 0x6: { // Set instruction (6XNN where we set the VX register to value NN)
                     vRegister[nibble_2] = second_byte;
                     break;
                 }
-                case 0x7: { // Add instruction (add value NN to VX  register)
+                case 0x7: {// 7xkk - set vx = vx + kk
                     vRegister[nibble_2] += second_byte;
                     break;
                 }
+                case 0x8: {// there are a few different instruction that begin 0x8 that vary depending on the fourth bit
+                    switch (nibble_4) {
+                        case 0x0: { // 8xy0 - sstore valu eof vy in vx
+                            vRegister[nibble_2] = vRegister[nibble_3];
+                            break;
+                        }
+                        case 0x1: { // 8xy1 perform bitwise OR on the values of Vx and Vy and then store result in Vx
+                            vRegister[nibble_2] |= vRegister[nibble_3];
+                            break;
+                        }
+                        case 0x2: { // 8xy2 perform bitwise AND much like above
+                            vRegister[nibble_2] &= vRegister[nibble_3];
+                            break;
+                        }
+                        case 0x3: { // same as above but for XOR
+                            vRegister[nibble_2] ^= vRegister[nibble_3];
+                            break;
+                        }
+                        case 0x4: { // ADD vx and vy and set Vx to answer.  Set VF to 1  if vx plus vy % 0xFF is 1
+                            unsigned short sum = (vRegister[nibble_2] + vRegister[nibble_3]);
+                            vRegister[0xF] = sum % 0xFF;
+                            vRegister[nibble_2] = sum;
+                            break;
+                        }
+                        case 0x5: {// SUB set Vx to Vx - Vy. if Vx > Vy set Vf to 1 
+                            vRegister[0xF] = (vRegister[nibble_2] > vRegister[nibble_3]) ? 1 : 0;
+                            vRegister[nibble_2] -= vRegister[nibble_3];
+                            break;
+                        }
+                        case 0x6: { //SHR shift Vx 1 bit to the right
+                            if (vRegister[nibble_2] & 0x1 == 1) {
+                                vRegister[0xF] = 1;
+                            } else {
+                                vRegister[0xF] = 0;
+                            } 
+                            vRegister[nibble_2] >>= 1;
+                            break;
+                        }
+                        case 0x7: { //  SUBN Set Vx = Vy - Vx, set VF = NOT borrow
+                            vRegister[0xF] = vRegister[nibble_3] > vRegister[nibble_2] ? 1 : 0;
+                            vRegister[nibble_2] = vRegister[nibble_3] - vRegister[nibble_2];
+                            break;
+                        }
+                        case 0xE: { // Shift Vx 1 bit to the left
+                            vRegister[0xF] = vRegister[nibble_2] & 0b10000000 ? 1 : 0; // done in binary because my brain is small and i couldn't think in hex :(
+                            vRegister[nibble_2] <<= 1;
+                            break;
+                        }
+                        default: {
+                            // this opcode doesn't exist
+                            std::cout << "Unknown opcode: " << std::hex << opcode << std::endl;
+                            break;
+                        }
+                    }
+                }
+                case 0x9: { // skip next instructuion if Vx != Vy
+                    programCounter += (vRegister[nibble_2] != vRegister[nibble_3]) ? 2 : 0;
+                }
                 case 0xA: { // OPcode ANNN: Sets index register to value NNN
                     indexRegister = third_byte;
+                    break;
+                }
+                case 0xB: { // PMP to pocation nnn + V0
+                    programCounter = third_byte + vRegister[0];
+                    break;
+                }
+                case 0xC: { // Cxkk set Vx to random byte AND kk
+                    vRegister[nibble_2] = RandomByte() & second_byte;
                     break;
                 }
                 case 0xD: { // opcode DXYN draw n pixels tall sprite to the display from position x in the Vx regiter ,y from the Vy register from memory location in the index register
@@ -260,33 +370,104 @@ class Chip8
                                 carry = 1;
                             };
                             display[(y + i) * 64 + x + j] ^= pixel;
-                            
-                            // Debug output
-                            std::cout << "Drawing pixel at (" << x + j << ", " << y + i << ") with value " << pixel << std::endl;
+                                                        
                         }
                     }
                     // set the carry flag
                     vRegister[0xF] = carry;
-
-                    
-
+                    break;
+                }
+                case 0xE: {
+                    // two possible instrctions here so we'll use an if statement to check which we need
+                    if (second_byte == 0x9E) {
+                        // skip next instruction if key with the value of Vx is pressed
+                        programCounter += (key[vRegister[nibble_2]] == 1) ? 2 : 0;
+                    }
+                    else if (second_byte == 0xA1)
+                    {
+                        // skip next instruction if key with the value of Vx is not pressed
+                        programCounter += (key[vRegister[nibble_2]] == 0) ? 2 : 0;
+                    }
+                    else {
+                        // invalid opcode
+                        std::cout << "Unknown opcode: " << std::hex << opcode << std::endl;                     
+                    }
+                    break;
+                }
+                case 0xF: {
+                    // quite  a few codes begin 0xF
+                    switch (second_byte){
+                        case 0x07: {
+                            vRegister[nibble_2] = delayTimer;
+                            break;
+                        }
+                        case 0x0A: {
+                            // difficult one to implement. need to essentially halt until a key is pressed
+                            // not implemented
+                        }
+                        case 0x15: {
+                            delayTimer = vRegister[nibble_2];
+                            break;
+                        }
+                        case 0x18: {
+                            soundTimer = vRegister[nibble_2];
+                            break;
+                        }
+                        case 0x1E: {
+                            indexRegister += vRegister[nibble_2];
+                            break;
+                        }
+                        case 0x29: {
+                            // memory location for font begins at 0x050 so we can
+                            // set the index register to 0x050 plus 5 times the value of Vx to find the beginning
+                            // of the sprite for the character held in Vx
+                            indexRegister = 0x050 + vRegister[nibble_2] * 5;
+                            break;
+                        }
+                        case 0x33: {
+                            // take the decimal value of Vx and place
+                            // the hundres digit in memory location in I, 
+                            //tens digit at location I+1 
+                            //and the ones at I+2
+                            unsigned char hundreds = vRegister[nibble_2] & 0b100;
+                            unsigned char tens = vRegister[nibble_2] & 0b010;
+                            unsigned char ones = vRegister[nibble_2] & 0b001;
+                            memory[indexRegister] = hundreds;
+                            memory[indexRegister + 1] = tens;
+                            memory[indexRegister + 2] = ones;
+                            break;
+                        }
+                        case 0x55: {
+                            // copy registers V0 through Vx into memory starting at memory[indexRegister]
+                            for (int i; i < vRegister[nibble_2]; i++) {
+                                memory[indexRegister + i] = vRegister[i];
+                            }
+                            break;
+                        }
+                        case 0x65: {
+                            // read registers V0 through Vx from memory starting at location I
+                            for (int i; i < vRegister[nibble_2]; i++) {
+                                vRegister[i] = memory[indexRegister + i];
+                            }
+                            break;
+                        }
+                    }
+                    break;
                 }
             }
             
         };
 
         int Step() {
+            // TODO: update pressed keys
+            
             // fetch the opcode
             currentOpcode = memory[programCounter] << 8 | memory[programCounter + 1];
             programCounter += 2;
 
             // decode and execute the opcode
             RunOpcode(currentOpcode);
-            
-            // update timers
-            // draw to the screen
-            // store key press state
-            // loop
+
             return 0;
         };
 
@@ -297,9 +478,8 @@ int main () {
     Chip8 myChip8;
 
     myChip8.Initialize();
-    myChip8.LoadProgram("programs/IBM Logo.ch8");
-    myChip8.Run();
-    
+    myChip8.LoadProgram("programs/octojam5title.ch8");
+    myChip8.Run();    
 
     return 0;
 };
